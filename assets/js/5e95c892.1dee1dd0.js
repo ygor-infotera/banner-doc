8b1d9572e93d6c96bd95db23d481bee8e54773a3
ygor-infotera <ygor-infotera@users.noreply.github.com>
2026-01-19 19:01:32 +0000

    deploy: f94e6d1bf20c7d4435dd3f89ba172955814aef32

---
+++ assets/js/5e95c892.1dee1dd0.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkwebsite=globalThis.webpackChunkwebsite||[]).push([[647],{9250(e,s,r){r.r(s),r.d(s,{default:()=>h});r(7140);var a=r(3526),t=r(5120),u=r(2595),c=r(8713),i=r(3937),l=r(5656);function h(e){return(0,l.jsx)(u.e3,{className:(0,a.A)(t.G.wrapper.docsPages),children:(0,l.jsx)(i.A,{children:(0,c.v)(e.route.routes)})})}}}]);
