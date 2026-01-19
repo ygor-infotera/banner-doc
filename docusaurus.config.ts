fab6d54acfc1ae37b9c28f50dfda2dc52f68a02b
Ygor Perez <ygor@infotera.com.br>
2026-01-19 15:54:25 -0300

    prepare for deploy

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,16 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://ygor-infotera.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/banner-doc/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "ygor-infotera", // Usually your GitHub org/user name.
+  projectName: "banner-doc", // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: "throw",
 
@@ -42,8 +43,7 @@ const config: Config = {
         docs: {
           sidebarPath: "./sidebars.ts",
           routeBasePath: "/", // Serve the docs at the site's root
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/ygor-infotera/banner-doc/tree/main/",
         },
         blog: false, // Remove blog functionality
         theme: {
@@ -82,7 +82,7 @@ const config: Config = {
           items: [
             {
               label: "Integração Banners",
-              to: "/banner-v2",
+              to: "/",
             },
           ],
         },
